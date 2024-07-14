80c451c4ca270c7055cf28f5ddcc1e519c2e264c
fThAbhishek-Pandey <abhishek830564@email.com>
2024-07-14 14:52:55 +0530

    first commit

---
+++ Day2/operator.js
@@ -0,0 +1,59 @@
+let vari1 =8, vari2= 4;
+// task1
+let vari3 = vari1 + vari2;
+console.log("+ : ",vari3);
+//  task2
+vari3 = vari1 - vari2;
+console.log("- : ",vari3);
+//  task3
+vari3 = vari1 * vari2;
+console.log("* : ",vari3);
+//  task4
+vari3 = vari1 / vari2;
+console.log("/ : ",vari3);
+//  task5
+vari3 = vari1 % vari2;
+console.log("% : ",vari3);
+// Assignment operater
+//  task6
+vari3 += vari1 + vari2;
+console.log("+= : ",vari3);
+//  task7
+vari3 -= vari1 + vari2;
+console.log("-= : ",vari3);
+//  comparision operater
+//  task8
+vari3 = vari1 > vari2;
+console.log("> : ",vari3);
+vari3 = vari1 < vari2;
+console.log("< : ",vari3);
+//  task9
+vari3 = vari1 >= vari2;
+console.log(">= : ",vari3);
+vari3 = vari1 <= vari2;
+console.log("<= : ",vari3);
+//  task10
+vari1="6", vari2=6;
+vari3 = vari1 == vari2;
+console.log("== ",vari3);
+vari3 = vari1 === vari2;
+console.log("=== : ",vari3);
+//  logical operater
+//  task11
+vari1= true, vari2=false;
+vari3 = vari1 && vari2;
+console.log("&& : ",vari3);
+//  task12
+vari3 = vari1 || vari2;
+console.log("||",vari3);
+//  task13
+vari3 = !vari1;
+console.log("! : ",vari3);
+// ternary operater
+//  task14
+var1 =8, vari2= 4;
+let flag = true;
+vari3 = flag ? vari1 + vari2 : vari1*vari2;
+console.log(vari3);
+
+
---
+++ day1/Activity01.js
@@ -0,0 +1,33 @@
+// activity 1
+
+var vari1= 15;
+console.log(vari1);
+let vari2= "abhishek";
+console.log(vari2);
+//  activty 2
+
+const vari3 = true;
+console.log(vari3);
+//  activty 3
+let vari4 =9;
+console.log(typeof vari4);
+let vari5 ="abhishek";
+console.log(typeof vari5);
+let vari6 = "true";
+console.log(typeof vari6);
+let vari7 = [8,3,0,5,6,4];
+console.log(typeof vari7);
+let Abhishek = {
+    learn : "b.tech in IT",
+    dost: "kirti",
+    age : 21,
+    love : "Royel pandit"
+}
+console.log(typeof Abhishek);
+// Activity 4
+let vari8= 9;
+console.log(vari8);
+vari8= "abhishek";
+console.log(vari8);
+vari8= Abhishek;
+console.log(vari8);
